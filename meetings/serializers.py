e05e5f94f346fa82dd5e20b649b2acc6ecbf304c
Dorvin <ladig1017@gmail.com>
2019-03-24 20:31:44 +0900

    updated test and other codes

--- meetings/serializers.py
+++ meetings/serializers.py
@@ -5,16 +5,21 @@ from django.contrib.auth.models import User
 
 
 class MeetingSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = serializers.ReadOnlyField(source='user.pk')
     
     def validate(self, attrs):
-        if attrs['sinceWhen'] > attrs['tilWhen']:
+        if attrs['sinceWhen'] >= attrs['tilWhen']:
             raise serializers.ValidationError("since time is late than untill time")
         meetings = Meeting.objects.all()
         for meeting in meetings:
-            if meeting.sinceWhen < attrs['sinceWhen'] and attrs['sinceWhen'] < meeting.tilWhen:
+            if self.instance != None:
+                if meeting.id == self.instance.id:
+                    continue
+            if meeting.sinceWhen <= attrs['sinceWhen'] and attrs['sinceWhen'] < meeting.tilWhen:
                 raise serializers.ValidationError("there is already existing meeting on the time you want")
-            if meeting.sinceWhen < attrs['tilWhen'] and attrs['tilWhen'] < meeting.tilWhen:
+            if meeting.sinceWhen < attrs['tilWhen'] and attrs['tilWhen'] <= meeting.tilWhen:
+                raise serializers.ValidationError("there is already existing meeting on the time you want")
+            if meeting.sinceWhen > attrs['sinceWhen'] and attrs['tilWhen'] > meeting.tilWhen:
                 raise serializers.ValidationError("there is already existing meeting on the time you want")
         return attrs
 
